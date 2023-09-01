6084d270ff138110e24ee17c4cf68a9f5809650b
Joseph <JosephSped23@gmail.com>
2023-08-31 22:24:15 -0400

    creates functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name : "Sam" ,
+    streetAddress : "11 Broadway"
+};
+
+function updateEmployeeWithKeyAndValue (employee , key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+/// destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "12 Broadway");
+
+function deleteFromEmployeeByKey (employee , name) {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+   delete employee.name;
+   return employee;
+}
+
+// let newEmployee = destructivelyDeleteFromEmployeeByKey("employee", "name");
